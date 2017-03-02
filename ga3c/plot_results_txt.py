d8d9c275f30b88f86f4efff4a378cdb42c4ac198
etienne-perot <etienne.perot@valeo.com>
2017-03-02 15:10:51 +0100

    sync plotter with lstm version

--- ga3c/plot_results_txt.py
+++ ga3c/plot_results_txt.py
@@ -25,17 +25,31 @@ def main():
     time = []
     sh0 = hour[0].split(' ')[1].split(':')
     t0 = int(sh0[0]) + float(sh0[1])/60.0 + float(sh0[2])/3600.0
+    tprev = t0
+    total_time = 0
+    dt = 0
     for h in hour:
-        sh = h.split(' ')[1].split(':')
-        t = int(sh[0]) + float(sh[1])/60.0 + float(sh[2])/3600.0 - t0
+        sh = h.split(' ')[1].split(':')  
+        tt = int(sh[0]) + float(sh[1])/60.0 + float(sh[2])/3600.0
+        if tt < tprev: 
+            dt = total_time
+            t0 = tt
+        elif (tt-tprev) > 0.5:
+            dt = total_time
+            t0 = tt
+        else:
+            total_time += (tt-tprev)
+        t = tt - t0 + dt
         time.append(t)
+        tprev = tt
+
     
     smoothing = 100
     reward_smooth = np.convolve(reward, np.ones((smoothing))/float(smoothing), 'same')
     
     xaxis = time
     plt.plot(xaxis, reward, color='b', alpha=0.2)
-    plt.plot(xaxis, reward_smooth, label='reward', color='b')
+    plt.plot(xaxis, reward_smooth, color='b')
     
     plt.xlabel('hours')
     plt.ylabel('score')
